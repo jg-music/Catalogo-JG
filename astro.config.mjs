2110ce63eeeeb7b36851e0ce155727447ebd8e77
JG Music Production <jgmusicprodution@gmail.com>
2026-07-12 23:53:31 -0500

    Corregir despliegue en Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jgmusicproduction.com",
   output: "static",
-  adapter: vercel(),
-  integrations: [react(), sitemap(), keystatic()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
